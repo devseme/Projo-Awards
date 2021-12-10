7cce3fd51f60a4ba18a152ba3f4072bb73e7cfbd
devseme <ian.ochenge@student.moringaschool.com>
2021-12-10 13:36:00 +0300

    created DB ace and updated settings

--- awardske/settings.py
+++ awardske/settings.py
@@ -87,8 +87,10 @@ WSGI_APPLICATION = 'awardske.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ace',
+        'USER': 'moringa',
+    'PASSWORD':'moringa',
     }
 }
 
@@ -117,7 +119,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
