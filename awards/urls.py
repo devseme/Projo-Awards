b7769d12e134b4fcfd7568410b4c409d0c26bf84
devseme <ian.ochenge@student.moringaschool.com>
2021-12-13 21:43:22 +0300

    solved log in bug

--- awards/urls.py
+++ awards/urls.py
@@ -5,6 +5,7 @@ urlpatterns=[
     # url('^$',views.welcome,name = 'welcome'),
     path('',views.index,name= 'index'),
     path('profile/', views.profile, name='profile'),
+    path('accounts/profile/', views.index,name='profile'),
     path('upload/project/', views.upload, name = "upload"),
     path('update_profile/<int:id>',views.update_profile, name='update_profile'),
     path('create_profile/',views.create_profile,name = 'create_profile'),
