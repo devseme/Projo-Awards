107fbb73b9d1b9c9edf4a59cfe9fda476563f883
devseme <ian.ochenge@student.moringaschool.com>
2021-12-13 23:59:09 +0300

    worked on classes tests

--- awards/tests.py
+++ awards/tests.py
@@ -1,3 +1,63 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Project, Profile
 
 # Create your tests here.
+class ProjectTestClass(TestCase):  # Project class test
+    def setUp(self):
+        # creating a user
+        user = User.objects.create(
+            username="test_user", first_name="creative", last_name="awards"
+        )
+
+        self.project = Project(
+            name="Test Name",
+            description="Test Description",
+            image="image.jpg",
+            user=user,
+        )
+    def test_instance(self):
+        self.assertTrue(isinstance(self.project, Project))
+    
+    def test_save_method(self):
+        self.project.save_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects) > 0)
+
+
+    def test_delete_method(self):
+        self.project.save_project()
+        self.project.delete_project()
+        projects = Project.objects.all()
+        self.assertTrue(len(projects) == 0)
+
+         
+     
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        
+        user = User.objects.create(
+            username="test_user", first_name="creative", last_name="awards"
+        )
+
+        self.profile = Profile(
+            bio="Test Bio",
+            user=user,
+            contact="Test Number",
+        )
+
+    
+    def test_instance(self):
+        self.assertTrue(isinstance(self.profile, Profile))
+
+    def test_save_method(self):
+        self.profile.save_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+    def test_delete_method(self):
+        self.profile.save_profile()
+        self.profile.delete_profile()
+        profiles = Profile.objects.all()
+        self.assertTrue(len(profiles) == 0)
+
